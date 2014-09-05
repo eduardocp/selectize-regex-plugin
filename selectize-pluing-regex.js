289576c656c1a012cf875d5bb80d2f66dc1a16e5
Eduardo Carísio <eduardo_carisio@hotmail.com>
2014-09-05 14:04:45 -0300

    Add js file

---
+++ selectize-pluing-regex.js
@@ -0,0 +1,37 @@
+Selectize.define('regular_expression', function (options) {
+    if (this.settings.mode === 'single') return;
+
+    options = $.extend({
+        addRegex: null,
+        addRegexContainerError: null,
+        addRegexErrorMessage: null
+    }, options);
+
+    var self = this;
+
+    this.addOption = (function () {
+        var original = self.addOption;
+
+        return function () {
+            if (self.settings.addRegex != null && self.settings.addRegex != '') {
+                var regexp = self.settings.addRegex;
+                var correct = regexp.test(arguments[0].value);
+                if (self.settings.addRegexContainerError != null && self.settings.addRegexContainerError != '') {
+                    if (self.settings.addRegexErrorMessage != null && self.settings.addRegexErrorMessage != '') {
+                        $(self.settings.addRegexContainerError).html(self.settings.addRegexErrorMessage);
+                    }
+                }
+                if (!correct) return;
+                else {
+                    if (self.settings.addRegexContainerError != null && self.settings.addRegexContainerError != '') {
+                        if (self.settings.addRegexErrorMessage != null && self.settings.addRegexErrorMessage != '') {
+                            $(self.settings.addRegexContainerError).html("");
+                        }
+                    }
+                    $.post("/Movies/VerifyHour", { hour: arguments[0].value, room: parseInt() })
+                }
+            }
+            return original.apply(this, arguments);
+        };
+    })();
+});
